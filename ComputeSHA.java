89ee1dc8a5d88d670e272e4553701d182c165197
Jiayu Li <lijiayu@Jiayus-MacBook-Pro.local>
2016-01-11 16:14:29 -0800

    final

--- ComputeSHA.java
+++ ComputeSHA.java
@@ -6,7 +6,7 @@ public class ComputeSHA {
     String filename;
 
 	public static void main (String args[]) {
-        
+        try {
         if (args.length == 0) {
             System.out.println("Please input filename");
             return;
@@ -15,18 +15,32 @@ public class ComputeSHA {
             ComputeSHA cSHA = new ComputeSHA(args[0]);
             System.out.println(cSHA.go());
         }
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
 	}
     
     public ComputeSHA(String str) {
         filename = str;
     }
     
-    public String go() {
-        String input = readfile(filename);
-        return encryptSHA1(input);
+    public String go() throws Exception{
+        byte[] arr = readfile(filename);
+        return encryptSHA1(arr);
     }
     
-    public String readfile(String str) {
+    public byte[] readfile(String str) throws Exception{
+            InputStream is = new FileInputStream(filename);
+            byte[] buffer = new byte[1024];
+            MessageDigest md = MessageDigest.getInstance("SHA-1");
+            int numRead;
+            while ((numRead = is.read(buffer)) != -1) {
+                md.update(buffer, 0, numRead);
+            }
+            is.close();
+            return md.digest();
+        /*
         String input = "";
         try {
             BufferedReader br = new BufferedReader(new FileReader(filename));
@@ -40,18 +54,20 @@ public class ComputeSHA {
             ex.printStackTrace();
         }
         return input;
+         */
     }
     
-    public String encryptSHA1(String str) {
+    public String encryptSHA1(byte[] arr) {
         String result = "";
         try {
-        MessageDigest md = MessageDigest.getInstance("SHA-1");
-        byte[] arr = md.digest(str.getBytes());
-        StringBuffer sb = new StringBuffer();
-        for (int i = 0 ; i < arr.length ; i++) {
-            sb.append(String.format("%02x", arr[i]));
-        }
-        result = sb.toString();
+            //byte[] arr = md.digest(str.getBytes());
+            StringBuffer sb = new StringBuffer();
+            for (int i = 0 ; i < arr.length ; i++) {
+                //sb.append(String.format("%1$02X", arr[i]));
+                sb.append(Integer.toString((arr[i] & 0xff) + 0x100, 16).substring(1));
+            }
+            result = sb.toString();
+             
         } catch (Exception ex) {
             ex.printStackTrace();
         }
